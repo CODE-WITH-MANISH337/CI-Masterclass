195e220e1cf0e2eb68b9de79fa4a7f71ac1a9a9c
Manish <manishjaiswar1221@gmail.com>
2026-06-08 09:48:51 +0530

    Project add to the app

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title('Power Calculator')
+st.subheader('calculate the square cube and fifth power of input number')
+
+
+value=st.number_input("enter a number",value=1,step=1)
+
+if value:
+    st.write(f'The Square is {value**2}')
+    st.write(f'The Cube is {value **3}')
+    st.write(f'The Fifth is {value**5}')
